7529fcb71b8dbc879a3e1a092076a2c37efad593
developerpark2348 <developerpark2348@gmail.com>
2021-02-06 17:36:46 +0900

    react-native내에서 linearGradient(색상이 조금씩 바뀌는)사용법
    1. expo install expo-linear-gradient로 다운로드 한다.
    2. import해온다.
    3. 해당 예시폴더를 가져와 작동시킬 컴포넌트를 사이에 넣는다
    
    => function -> class로 형식을 바꾸어봄.

--- App.js
+++ App.js
@@ -1,40 +1,51 @@
-import React, { useEffect,useState } from 'react';
-import {Alert} from "react-native";
+import React from "react";
+import { Alert } from "react-native";
 import Loading from "./Loading";
-import Weather from './Weather';
 import * as Location from "expo-location";
-import { apisAreAvailable } from 'expo';
-import axios from 'axios';
+import axios from "axios";
+import Weather from "./Weather";
 
-const API_KEY = "e58807bd9d82787951a48c580505f37d";
+const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
 
-export default function App() {
-  let[로딩중, 로딩중변경] = useState(true)
-  let[데이터, 데이터변경] = useState([])
-
-  let getWeather = async(latitude, longitude) => {
-    const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`);
-    로딩중변경(false);
-    데이터변경(data);
+export default class extends React.Component {
+  state = {
+    isLoading: true
   };
-
-  let getLocation = async() => {
-    try{
+  getWeather = async (latitude, longitude) => {
+    const {
+      data: {
+        main: { temp },
+        weather
+      }
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+    );
+    this.setState({
+      isLoading: false,
+      condition: weather[0].main,
+      temp
+    });
+  };
+  getLocation = async () => {
+    try {
       await Location.requestPermissionsAsync();
-      const { coords: { latitude, longitude }} = await Location.getCurrentPositionAsync();
-      getWeather(latitude, longitude);
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
     } catch (error) {
-      Alert.alert("사용자의 위치를 알 수 없습니다.", "슬프네요")
+      Alert.alert("Can't find you.", "So sad");
     }
   };
-
-  useEffect(()=>{
-    getLocation();
-  }, [] );
-
-  return(
-  
-    로딩중? <Loading /> : <Weather temp={Math.round(데이터.main.temp)} condition={데이터.weather.main}/>
-
-  )
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    );
+  }
 }
--- Weather.js
+++ Weather.js
@@ -1,21 +1,124 @@
-import React from 'react';
-import { View, Text, StyleSheet } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function Weather(props){
-    return(
+const weatherOptions = {
+  Thunderstorm: {
+    iconName: "weather-lightning",
+    gradient: ["#373B44", "#4286f4"],
+    title: "천둥 벼락",
+    subtitle: "천둥 번개에요!! 나가지 마세요!"
+  },
+  Drizzle: {
+    iconName: "weather-hail",
+    gradient: ["#89F7FE", "#66A6FF"],
+    title: "뭐여",
+    subtitle: "이런 날씨가 실제로 오다니"
+  },
+  Rain: {
+    iconName: "weather-rainy",
+    gradient: ["#00C6FB", "#005BEA"],
+    title: "비가 와요",
+    subtitle: "비가 내려요! 우산 꼭 챙겨가세요!"
+  },
+  Snow: {
+    iconName: "weather-snowy",
+    gradient: ["#7DE2FC", "#B9B6E5"],
+    title: "눈이 와요",
+    subtitle: "눈이에요! 눈사람 만들어야죠!"
+  },
+  Atmosphere: {
+    iconName: "weather-hail",
+    gradient: ["#89F7FE", "#66A6FF"],
+    title: "뭐여",
+    subtitle: "이런 날씨가 실제로 오다니"
+  },
+  Clear: {
+    iconName: "weather-sunny",
+    gradient: ["#FF7300", "#FEF253"],
+    title: "맑음",
+    subtitle: "맑고 화창한 날! 나가셔도 괜찮겠어요"
+  },
+  Clouds: {
+    iconName: "weather-cloudy",
+    gradient: ["#D7D2CC", "#304352"],
+    title: "구름 잔뜩",
+    subtitle: "구름 가득낀 날씨 햇볕을 볼수없어요"
+  },
+  Mist: {
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", "#D39D38"],
+    title: "안개 뿌옇",
+    subtitle: "안개로 인해 운전 조심하세요!"
+  },
+  Dust: {
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", "#D39D38"],
+    title: "미세 먼지!!",
+    subtitle: "마스크 꼭 챙기세요!"
+  },
+  Haze: {
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", "#D39D38"],
+    title: "뭐여",
+    subtitle: "밖에 나가지 마세요.."
+  }
+};
 
-        <View style={styles.container}>
-            <Text>{props.temp}</Text>
-        </View>
-
-    )
+export default function Weather({ temp, condition }) {
+  return (
+    <LinearGradient
+      colors={weatherOptions[condition].gradient}
+      style={styles.container}
+    >
+      <StatusBar barStyle="light-content" />
+      <View style={styles.halfContainer}>
+        <MaterialCommunityIcons
+          size={96}
+          name={weatherOptions[condition].iconName}
+          color="white"
+        />
+        <Text style={styles.temp}>{temp}°</Text>
+      </View>
+      <View style={{ ...styles.halfContainer, ...styles.textContainer }}>
+        <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+        <Text style={styles.subtitle}>
+          {weatherOptions[condition].subtitle}
+        </Text>
+      </View>
+    </LinearGradient>
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  temp: {
+    fontSize: 42,
+    color: "white"
+  },
+  halfContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  title: {
+    color: "white",
+    fontSize: 44,
+    fontWeight: "300",
+    marginBottom: 10
+  },
+  subtitle: {
+    fontWeight: "600",
+    color: "white",
+    fontSize: 24
+  },
+  textContainer: {
+    paddingHorizontal: 20,
+    alignItems: "flex-start"
+  }
+});
