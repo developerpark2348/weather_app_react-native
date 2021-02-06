f6531bf1f79118f7854e0e4d2de23bdcde1b8e71
developerpark2348 <developerpark2348@gmail.com>
2021-02-06 12:47:12 +0900

    로딩창 디자인
    add Loading components

--- Loading.js
+++ Loading.js
@@ -1,2 +1,26 @@
 import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
+
+export default function Loading(){
+    return (
+
+    <View style={styles.container}>
+        <Text style={styles.text}>날씨 알아보기</Text>
+    </View>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "flex-end",
+        paddingHorizontal: 30,
+        paddingVertical: 100,
+        backgroundColor: "#FDF6AA"
+    },
+    text: {
+        color: "#2c2c2c",
+        fontSize: 30
+    }
+})
